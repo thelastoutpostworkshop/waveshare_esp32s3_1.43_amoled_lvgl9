3260294d20eaef007b1dd6eb7b9853bd83cb2f35
thelastoutpostworkshop <thelastoutpostworkshop@gmail.com>
2025-09-06 17:31:55 -0400

    ui

--- ui_Screen1.c
+++ ui_Screen1.c
@@ -10,6 +10,7 @@ lv_obj_t * uic_Screen1;
 lv_obj_t * ui_Screen1 = NULL;
 lv_obj_t * ui_Image3 = NULL;
 lv_obj_t * ui_Image2 = NULL;
+lv_obj_t * ui_Label1 = NULL;
 // event funtions
 
 // build funtions
@@ -33,8 +34,19 @@ void ui_Screen1_screen_init(void)
     lv_obj_set_width(ui_Image2, LV_SIZE_CONTENT);   /// 1
     lv_obj_set_height(ui_Image2, LV_SIZE_CONTENT);    /// 1
     lv_obj_set_align(ui_Image2, LV_ALIGN_CENTER);
-    lv_obj_add_flag(ui_Image2, LV_OBJ_FLAG_CLICKABLE);     /// Flags
-    lv_obj_remove_flag(ui_Image2, LV_OBJ_FLAG_SCROLLABLE);      /// Flags
+    lv_obj_remove_flag(ui_Image2, LV_OBJ_FLAG_PRESS_LOCK | LV_OBJ_FLAG_CLICK_FOCUSABLE | LV_OBJ_FLAG_GESTURE_BUBBLE |
+                       LV_OBJ_FLAG_SNAPPABLE | LV_OBJ_FLAG_SCROLLABLE);     /// Flags
+
+    ui_Label1 = lv_label_create(ui_Screen1);
+    lv_obj_set_width(ui_Label1, LV_SIZE_CONTENT);   /// 1
+    lv_obj_set_height(ui_Label1, LV_SIZE_CONTENT);    /// 1
+    lv_obj_set_x(ui_Label1, 1);
+    lv_obj_set_y(ui_Label1, -133);
+    lv_obj_set_align(ui_Label1, LV_ALIGN_CENTER);
+    lv_label_set_text(ui_Label1, "Surface Level");
+    lv_obj_remove_flag(ui_Label1, LV_OBJ_FLAG_PRESS_LOCK | LV_OBJ_FLAG_CLICK_FOCUSABLE | LV_OBJ_FLAG_GESTURE_BUBBLE |
+                       LV_OBJ_FLAG_SNAPPABLE);     /// Flags
+    lv_obj_set_style_text_font(ui_Label1, &lv_font_montserrat_20, LV_PART_MAIN | LV_STATE_DEFAULT);
 
     uic_Screen1 = ui_Screen1;
     uic_bubble = ui_Image2;
@@ -51,5 +63,6 @@ void ui_Screen1_screen_destroy(void)
     ui_Image3 = NULL;
     uic_bubble = NULL;
     ui_Image2 = NULL;
+    ui_Label1 = NULL;
 
 }
--- ui_Screen1.h
+++ ui_Screen1.h
@@ -16,6 +16,7 @@ extern void ui_Screen1_screen_destroy(void);
 extern lv_obj_t * ui_Screen1;
 extern lv_obj_t * ui_Image3;
 extern lv_obj_t * ui_Image2;
+extern lv_obj_t * ui_Label1;
 // CUSTOM VARIABLES
 extern lv_obj_t * uic_Screen1;
 extern lv_obj_t * uic_bubble;
